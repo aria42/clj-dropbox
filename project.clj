f565ebc01483b2f5d1804c00f226877e35f25c0e
Aria Haghighi <aria42@gmail.com>
2010-08-20 13:05:02 -0400

    Bumped version. On clojure 1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.aria42/clj-dropbox "0.1"
+(defproject org.aria42/clj-dropbox "0.2-SNAPSHOT"
   :decription "Clojure bindings to Dropbox API"
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]
-		             [clj-oauth "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		         [clj-oauth "1.2.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
